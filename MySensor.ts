c88505faa56b97b902c671df1daaffa3b6171d2e
myrobotbit <41653060+myrobotbit@users.noreply.github.com>
2018-10-18 11:37:15 +0700

    Update MySensor.ts

--- MySensor.ts
+++ MySensor.ts
@@ -24,29 +24,29 @@ namespace MySensor {
 
      /**
      * Read Analog Sensor P0-P4,P10 
-     * @param pin sensor's active pin
+     * @param pinCH sensor's active pin
      * @return number returns analog value from 0 to 1023
      */
     //% blockId="MySensor_readSensor" block="analog sensor %sensorSEL"
     //% weight=100
-    export function readSensor(pin:sensorSEL): number{
+    export function readSensor(pinCH:sensorSEL): number{
 
-	if(pin == sensorSEL.S0){
+	if(pinCH == sensorSEL.S0){
 	    return pins.analogReadPin(AnalogPin.P0)
         }
-	if(pin == sensorSEL.S1){
+	if(pinCH == sensorSEL.S1){
 	    return pins.analogReadPin(AnalogPin.P1)
         }
-	if(pin == sensorSEL.S2){
+	if(pinCH == sensorSEL.S2){
 	    return pins.analogReadPin(AnalogPin.P2)
         }
-	if(pin == sensorSEL.S3){
+	if(pinCH == sensorSEL.S3){
 	    return pins.analogReadPin(AnalogPin.P3)
         }
-	if(pin == sensorSEL.S4){
+	if(pinCH == sensorSEL.S4){
 	    return pins.analogReadPin(AnalogPin.P4)
         }
-	if(pin == sensorSEL.S10){
+	if(pinCH == sensorSEL.S10){
 	    return pins.analogReadPin(AnalogPin.P10)
         }
     }
