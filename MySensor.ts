33dab8155a1dffaf0278022d119e7983b51b148e
myrobotbit <41653060+myrobotbit@users.noreply.github.com>
2018-10-23 20:17:48 +0700

    Update MySensor.ts

--- MySensor.ts
+++ MySensor.ts
@@ -27,24 +27,24 @@ namespace MySensor {
     //% blockId="Mysensor_analog"  block="read analog sensor |%AnalogSensor|"
     //% weight=90
    export function analogREAD(analogSelect:AnalogSensor): number {
- 
+ 	let ADCValue:number
 	  if (analogSelect==AnalogSensor.S0) {
-          return pins.analogReadPin(AnalogPin.P0)
+          	return pins.analogReadPin(AnalogPin.P0)
 	  }
 	  if (analogSelect==AnalogSensor.S1) {
-          return pins.analogReadPin(AnalogPin.P1)
+          	return pins.analogReadPin(AnalogPin.P1)
 	  }
 	  if (analogSelect==AnalogSensor.S2) {
-          return pins.analogReadPin(AnalogPin.P2)
+          	return pins.analogReadPin(AnalogPin.P2)
 	  }
 	  if (analogSelect==AnalogSensor.S3) {
-          return pins.analogReadPin(AnalogPin.P3)
+          	return pins.analogReadPin(AnalogPin.P3)
 	  }
 	  if (analogSelect==AnalogSensor.S4) {
-          return pins.analogReadPin(AnalogPin.P4)
+          	return pins.analogReadPin(AnalogPin.P4)
 	  }
 	  if (analogSelect==AnalogSensor.S10) {
-          return pins.analogReadPin(AnalogPin.P10)
+          	return pins.analogReadPin(AnalogPin.P10)
 	  }
   }
 }
