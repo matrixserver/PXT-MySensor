7ec0c492bb133f3eefeec0e9733795af99b341e6
myrobotbit <41653060+myrobotbit@users.noreply.github.com>
2018-10-24 18:23:28 +0700

    Update MySensor.ts

--- MySensor.ts
+++ MySensor.ts
@@ -13,6 +13,21 @@ namespace MySensor {
         P10
     }
 
+        export enum digitalPins {
+        P0,
+        P1,
+        P2,
+        P3,
+        P4,
+        P10,
+        P6,
+        P7,
+        P8,
+        P9,
+        P12,
+        P5,
+        P11
+    }
 
     /**
      * read analog sensor value from P0 -P4 and P10
@@ -36,6 +51,47 @@ namespace MySensor {
                 return pins.analogReadPin(AnalogPin.P4);
             case analogPins.P10:
                 return pins.analogReadPin(AnalogPin.P10);
+            default:
+                return 0;
+        }
+    }
+
+    /**
+     * read Digital sensor value from P0 -P12
+     * @param selectpin         select digital pin to read
+     * @return number           returns analog value  0 or 1
+     */
+    //% blockId=MySensor_digitalRead
+    //% block="digital read sensor |%selectpin|"
+    //% weight=80
+    export function digitalRead(selectpin:digitalPins): number {
+        switch (selectpin) {
+            case digitalPins.P0:
+                return pins.digitalReadPin(DigitalPin.P0);
+            case digitalPins.P1:
+                return pins.digitalReadPin(DigitalPin.P1);
+             case digitalPins.P2:
+                return pins.digitalReadPin(DigitalPin.P2);
+              case digitalPins.P3:
+                return pins.digitalReadPin(DigitalPin.P3);
+              case digitalPins.P4:
+                return pins.digitalReadPin(DigitalPin.P04;
+              case digitalPins.P10:
+                return pins.digitalReadPin(DigitalPin.P10);
+              case digitalPins.P6:
+                return pins.digitalReadPin(DigitalPin.P6);
+              case digitalPins.P7:
+                return pins.digitalReadPin(DigitalPin.P7);
+              case digitalPins.P8:
+                return pins.digitalReadPin(DigitalPin.P8);
+              case digitalPins.P9:
+                return pins.digitalReadPin(DigitalPin.P9);
+              case digitalPins.P12:
+                return pins.digitalReadPin(DigitalPin.P12);
+              case digitalPins.P5:
+                return pins.digitalReadPin(DigitalPin.P5);
+              case digitalPins.P11:
+                return pins.digitalReadPin(DigitalPin.P11);
             default:
                 return 0;
         }
