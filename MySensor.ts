2605b829475a5e7e4b9da16f5b27661a913f89c3
myrobotbit <41653060+myrobotbit@users.noreply.github.com>
2018-10-24 21:57:43 +0700

    Update MySensor.ts

--- MySensor.ts
+++ MySensor.ts
@@ -4,7 +4,7 @@
 //% weight=10 icon="\uf11e" color=#339933 block="MySensor"
 namespace MySensor {
 
-   export enum analogPins {
+   export enum analogPort {
         P0,
         P1,
         P2,
@@ -37,19 +37,19 @@ namespace MySensor {
     //% blockId=MySensor_analogRead
     //% block="analog read |%selectpin|"
     //% weight=80
-    export function analogRead(selectpin: analogPins): number {
+    export function analogRead(selectpin: analogPort): number {
         switch (selectpin) {
-            case analogPins.P0:
+            case analogPort.P0:
                 return pins.analogReadPin(AnalogPin.P0);
-            case analogPins.P1:
+            case analogPort.P1:
                 return pins.analogReadPin(AnalogPin.P1);
-            case analogPins.P2:
+            case analogPort.P2:
                 return pins.analogReadPin(AnalogPin.P2);
-            case analogPins.P3:
+            case analogPort.P3:
                 return pins.analogReadPin(AnalogPin.P3);
-            case analogPins.P4:
+            case analogPort.P4:
                 return pins.analogReadPin(AnalogPin.P4);
-            case analogPins.P10:
+            case analogPort.P10:
                 return pins.analogReadPin(AnalogPin.P10);
             default:
                 return 0;
@@ -76,16 +76,16 @@ namespace MySensor {
                 return pins.digitalReadPin(DigitalPin.P3);
               case digitalPort.P4:
                 return pins.digitalReadPin(DigitalPin.P4;
-              case digitalPort.P5:
-                return pins.digitalReadPin(DigitalPin.P5;
-              case digitalPort.P6:
+            case digitalPort.P5:
+                return pins.digitalReadPin(DigitalPin.P5);
+            case digitalPort.P6:
                 return pins.digitalReadPin(DigitalPin.P6);
-              case digitalPort.P7:
+             case digitalPort.P7:
                 return pins.digitalReadPin(DigitalPin.P7);
               case digitalPort.P8:
                 return pins.digitalReadPin(DigitalPin.P8);
               case digitalPort.P9:
-                return pins.digitalReadPin(DigitalPin.P9);
+                return pins.digitalReadPin(DigitalPin.P9;
               case digitalPort.P10:
                 return pins.digitalReadPin(DigitalPin.P10);
               case digitalPort.P11:
